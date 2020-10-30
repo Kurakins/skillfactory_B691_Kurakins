0157d1f02b07ce70d69099f742619942d61e1538
Kurakins <72132431+Kurakins@users.noreply.github.com>
2020-10-30 15:53:29 +0100

    Calculating area of figures

---
+++ Calculating.py
@@ -0,0 +1,24 @@
+from Calculate_area import Rectangle, Square, Circle
+
+# далее создаём два прямоугольника
+
+rect_1 = Rectangle(3, 4)
+rect_2 = Rectangle(12, 5)
+# вывод площади наших двух прямоугольников
+print(rect_1.get_area())
+print(rect_2.get_area())
+
+square_1 = Square(5)
+square_2 = Square(10)
+
+figures = [rect_1, rect_2, square_1, square_2]
+for figure in figures:
+    if isinstance(figure, Square):
+        print(figure.get_area_square())
+    else:
+        print(figure.get_area())
+
+circle_1 = Circle(5)
+circle_2 = Circle(10)
+print(circle_1.get_area_circle())
+print(circle_2.get_area_circle())
